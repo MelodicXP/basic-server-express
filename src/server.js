13e774d9e74744468d4e7fff8694fc88b3f048b1
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-01-15 14:40:27 -0600

    Add and test basic get route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Require use of libraries
 const express = require('express');
 const dotenv = require('dotenv');
 
@@ -8,10 +9,20 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000; // Fallback to 3000 is no variable in env file
 
+// Single instance of express
 const app = express();
 
+
+// Establish route
+app.get('/person', (req, res, next) => {
+  res.status(200).send('real data coming soon...');
+
+});
+
+// Start server
 function start() {
   app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 }
 
+// Export for use in other files
 module.exports = { start };
